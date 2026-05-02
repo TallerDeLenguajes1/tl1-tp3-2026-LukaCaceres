e4cb64aff7aed62e0bdb5c41f9e694980e52f9f4
luka <lukadayo03@gmail.com>
2026-05-01 21:05:28 -0300

    Leer nombres

--- ArregloDeNombres.c
+++ ArregloDeNombres.c
@@ -18,17 +18,22 @@ int main()
     */
 
     char buff [5][50];
-    char *  Cadenas[5];
+    char *  Personas[5];
     int tam = 0;
     for(int i=0; i<5;i++){
         printf("Ingrese un nombre");
         gets(buff[i]);
         tam = strlen(buff[i]);
-        Cadenas[i]= (char *) malloc(sizeof(char)*tam+1);
+        Personas[i]= (char *) malloc(sizeof(char)*(tam+1)); //asigno espacio en memoria para personas[i]
+        strcpy(Personas[i], buff[i]); //copio a personas[i] lo que estaba en buff[i]
         getchar(); 
     }
     for(int i=0; i<5;i++){
-        puts(Cadenas[i]);
+        puts(Personas[i]);
+    }
+
+    for(int i=0; i<5;i++){
+        free(Personas[i]);
     }
 
 
